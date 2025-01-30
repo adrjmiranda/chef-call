ed348a73787c41769c408d3d0cb13a2f783fe560
Adriano Miranda <adrjmiranda@gmail.com>
2025-01-29 23:39:16 -0300

    chore: added route to user registration page

--- app/routes/web/site.php
+++ app/routes/web/site.php
@@ -1,5 +1,7 @@
 <?php
 
 use App\Controllers\Site\HomeController;
+use App\Controllers\Site\RegisterController;
 
 $app->get('/', HomeController::class . ':index')->setName('home');
+$app->get('/register', RegisterController::class . ':index')->setName('user_register');
