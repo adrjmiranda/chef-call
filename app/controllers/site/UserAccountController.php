c70be4cc3fed2742c5482c76771fb6e9e98d7593
Adriano Miranda <adrjmiranda@gmail.com>
2025-01-31 00:27:22 -0300

    chore: created controller to handle the user account page

---
+++ app/controllers/site/UserAccountController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controllers\Site;
+
+use App\Core\BaseController;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+/**
+ * Controller responsible for the user account page
+ * 
+ * @package App\Controllers\Site
+ */
+class UserAccountController extends BaseController
+{
+  /**
+   * Method responsible for redefining the user_account view
+   *
+   * @param \Psr\Http\Message\ServerRequestInterface $request
+   * @param \Psr\Http\Message\ResponseInterface $response
+   * @param array $args
+   * @return Response
+   */
+  public function index(Request $request, Response $response, array $args = []): Response
+  {
+    $view = $this->render('site.user_account', []);
+    $response->getBody()->write($view);
+
+    return $response;
+  }
+}
